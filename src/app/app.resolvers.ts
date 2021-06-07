fa051fcc67d32767b6ad6f178bb54190e0705ef2
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-06-08 01:45:25 +0200

    feature/profile-dashboard

--- src/app/app.resolvers.ts
+++ src/app/app.resolvers.ts
@@ -8,19 +8,22 @@ import { ENV } from '@shared/constants';
 import { Environment } from '@shared/environment.model';
 import { UserDetails } from '@shared/shared.models';
 import LogRocket from 'logrocket';
+import { AuthService } from '@core/auth/auth.service';
+import { FuseNavigationItem } from '@fuse/components/navigation';
 
 @Injectable({
     providedIn: 'root'
 })
-export class InitialDataResolver implements Resolve<any>
-{
+export class InitialDataResolver implements Resolve<any> {
     private _apiUrl = this._environment.baseUrls.apiUrl;
 
     /**
      * Constructor
      */
-    constructor(private _httpClient: HttpClient, @Inject(ENV) private _environment: Environment)
-    {
+    constructor(
+        private _httpClient: HttpClient,
+        private _auth: AuthService,
+        @Inject(ENV) private _environment: Environment) {
     }
 
     // -----------------------------------------------------------------------------------------------------
@@ -33,8 +36,9 @@ export class InitialDataResolver implements Resolve<any>
      * @param route
      * @param state
      */
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<InitialData>
-    {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<InitialData> {
+        console.log('claims', this._auth.roles);
+
         // Fork join multiple API endpoint calls to wait all of them to finish
         return forkJoin([
             this._httpClient.get<any>('api/common/messages'),
@@ -45,17 +49,16 @@ export class InitialDataResolver implements Resolve<any>
                 .pipe(
                     map(response => {
                         const userDetails = response.data as UserDetails;
-                        console.log( 'UserDetails', userDetails, 'InitialDataResolver' );
 
                         // LogRocket for production only
-                        if ( this._environment.production ){
+                        if (this._environment.production) {
                             LogRocket.identify(userDetails.userLoginId, {
                                 name: `${userDetails.firstName} ${userDetails.lastName}`,
                                 email: userDetails.email
                             });
                         }
 
-                        return  {
+                        return {
                             id: userDetails.userLoginId,
                             email: userDetails.email,
                             name: `${userDetails.firstName} ${userDetails.lastName}`,
@@ -64,18 +67,33 @@ export class InitialDataResolver implements Resolve<any>
                         };
                     }))
         ]).pipe(
-            map(([messages, navigation, notifications, shortcuts, user]) => ({
+            map(([messages, navigation, notifications, shortcuts, user]) => {
+
+                let compactNavigation = navigation.compact as FuseNavigationItem[];
+
+                if (!this._auth.roles.includes('Admin')) {
+                    const features = ['apps.groups','example'];
+                    const filterFeature = (featureId: string, nav: FuseNavigationItem): boolean => nav.id !== featureId;
+
+                    for (const feature of features) {
+                        compactNavigation = compactNavigation.filter(n => filterFeature(feature, n))
+                    }
+                }
+
+                return ({
                     messages,
                     navigation: {
-                        compact   : navigation.compact,
-                        default   : navigation.default,
+                        compact: compactNavigation,
+                        default: navigation.default,
                         futuristic: navigation.futuristic,
                         horizontal: navigation.horizontal
                     },
                     notifications,
                     shortcuts,
                     user
-                })
+                });
+
+                } // close map
             )
         );
     }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -17,7 +17,7 @@ export const appRoutes: Route[] = [
     // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
     // path. Below is another redirection for that path to redirect the user to the desired
     // location. This is a small convenience to keep all main routes together here on this file.
-    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'dashboard'},
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'pages/profile/dashboard'},
 
     // Auth routes for guests
     {
