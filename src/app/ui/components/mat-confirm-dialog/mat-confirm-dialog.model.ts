61af4178fd3e3c1828abc4b53b366d79c18bb1fa
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-07-17 01:03:11 +0200

    Added: delete person

--- src/app/ui/components/mat-confirm-dialog/mat-confirm-dialog.model.ts
+++ src/app/ui/components/mat-confirm-dialog/mat-confirm-dialog.model.ts
@@ -2,6 +2,18 @@ export interface ConfirmDialogModel
 {
     title: string;
     message: string;
+    confirmCaption?: string;
+    cancelCaption?: string;
+}
+
+export class DefaultConfirmDialog implements ConfirmDialogModel
+{
     confirmCaption: string;
     cancelCaption: string;
+
+    constructor(public title: string, public message: string)
+    {
+        this.confirmCaption = 'Confirm';
+        this.cancelCaption = 'Cancel';
+    }
 }
