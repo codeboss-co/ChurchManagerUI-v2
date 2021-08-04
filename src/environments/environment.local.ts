aad269441a0a0a10a4bb900cfe53e339b9df5740
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-08-04 22:14:29 +0200

    Attempt to fix push notifications

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,16 @@
+import { Environment } from '@shared/environment.model';
+
+export const environment: Environment  = {
+    production: true,
+    baseUrls: {
+        signalRHub: 'http://localhost:5001/signalr-hub',
+        apiUrl: 'http://localhost:5001/api'
+    },
+    contentful: {
+        spaceId: 'od8d9ub5fano',
+        token: 'l7jzfAF0vA1o7DX0Bj0ecJr8yxrpIoGFmFinLYmy1wg'
+    },
+    push: {
+        publicKey: 'BGPiiyCFTaVDuHs8IL7DTzXLccf41JCevm1ke2Z4OYA_W3K5Vadsnq8I8si_4_h_wMhM7iSoMjta-TPc1bylR6s'
+    }
+};
