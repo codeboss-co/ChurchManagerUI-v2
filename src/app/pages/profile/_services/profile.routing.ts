b2ce8bdc5b471578c850206886dc57ee47cd4a22
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-08-07 18:44:07 +0200

    Added: followup profile page

--- src/app/pages/profile/_services/profile.routing.ts
+++ src/app/pages/profile/_services/profile.routing.ts
@@ -1,10 +1,22 @@
-import { Route } from '@angular/router';
+import { Route, Routes } from '@angular/router';
 import { ProfileComponent } from 'app/pages/profile/profile.component';
 import { ProfileDiscipleshipResolver, ProfileResolver } from './profile.resolvers';
 import { ProfileAboutComponent } from '../tabs/about/profile-about.component';
 import { ProfileGroupsComponent } from '../tabs/groups/groups.component';
 import { ProfileDiscipleshipComponent } from '../tabs/discipleship/profile-discipleship.component';
 import { ProfileMyDashboardComponent } from '../components/my-dashboard/my-dashboard.component';
+import { ProfileFollowUpComponent } from '../tabs/followup/follow-up.component';
+
+const children: Routes = [
+    {path: '', component: ProfileAboutComponent },
+    {path: 'groups', component: ProfileGroupsComponent },
+    {path: 'discipleship', component: ProfileDiscipleshipComponent,
+        resolve: {
+            programs: ProfileDiscipleshipResolver
+        }
+    },
+    {path: 'follow-up', component: ProfileFollowUpComponent }
+];
 
 export const profileRoutes: Route[] = [
     {path: 'dashboard', component: ProfileMyDashboardComponent, pathMatch : 'full' },
@@ -14,15 +26,7 @@ export const profileRoutes: Route[] = [
         resolve: {
             profile: ProfileResolver
         },
-        children: [
-            {path: '', component: ProfileAboutComponent },
-            {path: 'groups', component: ProfileGroupsComponent },
-            {path: 'discipleship', component: ProfileDiscipleshipComponent,
-                resolve: {
-                    programs: ProfileDiscipleshipResolver
-                }
-            }
-        ]
+        children
     },
     {
         path     : ':personId',
@@ -30,15 +34,7 @@ export const profileRoutes: Route[] = [
         resolve: {
             profile: ProfileResolver
         },
-        children: [
-            {path: '', component: ProfileAboutComponent },
-            {path: 'groups', component: ProfileGroupsComponent },
-            {path: 'discipleship', component: ProfileDiscipleshipComponent,
-                resolve: {
-                    programs: ProfileDiscipleshipResolver
-                }
-            },
-        ]
+        children
     },
 
 ];
